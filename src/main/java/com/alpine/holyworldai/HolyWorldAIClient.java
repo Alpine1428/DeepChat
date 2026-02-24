1fb77ff9fd8cc8a6e5a0102b40bbc8543b05b4f8
Alpine1428 <max240320@gmail.com>
2026-02-24 19:06:28 +0300

    Update HolyWorldAIClient.java

--- src/main/java/com/alpine/holyworldai/HolyWorldAIClient.java
+++ src/main/java/com/alpine/holyworldai/HolyWorldAIClient.java
@@ -1,38 +1,18 @@
 package com.alpine.holyworldai;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.minecraft.text.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HolyWorldAIClient implements ClientModInitializer {
 
-    public static boolean autoReply = true;
-    public static ChatMonitor monitor;
+    public static final Logger LOGGER =
+            LoggerFactory.getLogger("holyworldai");
 
     @Override
     public void onInitializeClient() {
 
-        monitor = new ChatMonitor();
+        LOGGER.info("=== HOLYWORLD AI LOADED SUCCESSFULLY ===");
 
-        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-
-            dispatcher.register(ClientCommandManager.literal("ai")
-
-                .then(ClientCommandManager.literal("start")
-                    .executes(ctx -> {
-                        autoReply = true;
-                        ctx.getSource().sendFeedback(Text.literal("§aAI ENABLED"));
-                        return 1;
-                    }))
-
-                .then(ClientCommandManager.literal("stop")
-                    .executes(ctx -> {
-                        autoReply = false;
-                        ctx.getSource().sendFeedback(Text.literal("§cAI DISABLED"));
-                        return 1;
-                    }))
-            );
-        });
     }
 }
